994a88001fd0f0d873038f705d6f2d7f70f7356e
RustamovRR <rustamovriskiddin123@gmail.com>
2022-03-09 23:08:08 +0500

    added 8-dars

---
+++ 8-dars/code.js
@@ -0,0 +1,21 @@
+// const hamburger = document.getElementById('hamburger')
+// const content = document.getElementById('content_wrapper')
+// const right = document.getElementById('right')
+
+// hamburger.addEventListener('click', function () {
+//     content.style.transform = 'translateX(0)'
+// })
+
+// // right.onclick = function () {
+// //     content.style.transform = 'translateX(-100%)'
+// // }
+
+// right.addEventListener('click', function () {
+//     content.style.transform = 'translateX(-100%)'
+// })
+
+const newTag = document.createElement('h1')
+document.body.appendChild(newTag)
+newTag.innerHTML = 'New h1 tag'
+
+document.write('ajskdhfkla klsdjfkl')

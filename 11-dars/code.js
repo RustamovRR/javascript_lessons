bcf19734cc59e6a0891882d4c574f23e204d23c9
RustamovRR <rustamovriskiddin123@gmail.com>
2022-03-18 16:59:37 +0500

    add 12-dars

--- 11-dars/code.js
+++ 11-dars/code.js
@@ -1,74 +1,23 @@
-const images = document.querySelectorAll('.cars')
-const prevButton = document.querySelector('.prev_button')
-const nextButton = document.querySelector('.next_button')
-const dotSpans = document.querySelectorAll('.dots span')
+const output = document.getElementById('output')
+const result = document.getElementById('result')
+const del = document.getElementById('del')
+const equal = document.getElementById('equal')
+const buttons = document.querySelectorAll('button')
 
-let index = 1
-// 1. opacity, display none
-function nextSlider() {
-    if (index === 0) {
-        images[0].style.display = 'none'
-    } else {
-        images[index].style.display = 'block'
-    }
-    dotSpans[index].classList.add('active_dot')
-    dotSpans[index - 1].classList.remove('active_dot')
-    index++
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        if (this.innerHTML !== 'AC' || this !== del || this !== equal) {
+            output.innerHTML += this.innerHTML
+            result.innerHTML = eval(output.innerHTML)
+        }
+
+        if (this.innerHTML === 'DEL') {
+            output.innerHTML = output.innerHTML.slice(0, -1)
+        }
+
+        if (this.innerHTML === 'AC') {
+            output.innerHTML = ''
+        }
+    })
 }
-// nextButton.addEventListener('click', () => {
-//     // console.log('next', index)
-// })
-
-function prevSlider() {
-    if (index >= 2) {
-        index--
-    }
-    images[index].style.display = 'none'
-    
-    if (index >= 1) {
-        dotSpans[index].classList.add('active_dot')
-        dotSpans[index + 1].classList.remove('active_dot')
-    }
-    console.log('prev', index)
-}
-
-// prevButton.addEventListener('click', () => {
-// })
-
-
-// 2. z-index
-// nextButton.addEventListener('click', () => {
-//     console.log('next', index)
-//     images[index].style.zIndex = index + 1
-//     // if (images.length === index) {
-//     //     images[0].style.zIndex = index + 1
-//     // }
-//     index++
-// })
-
-// prevButton.addEventListener('click', () => {
-//     console.log('prev', index)
-//     images[index].style.zIndex = index + 1
-//     // if (images.length === index) {
-//     //     images[0].style.zIndex = index + 1
-//     // }
-//     index--
-// })
-
-// 3 - transform
-// let transform = 100
-// nextButton.addEventListener('click', () => {
-//     for (let i = 0; i <= images.length; i++) {
-//         if (images[i].getAttribute('id') !== 'last_img') {
-//             images[i].style.transform = `translateX(-${transform}%)`
-//         }
-//     }
-//     transform += 100
-// })
-
-// prevButton.addEventListener('click', () => {
-//     for (let i = 0; i < images.length; i++) {
-//         images[i].style.transform = `translateX(${transform}%)`
-//     }
-//     transform += 100
-// })
